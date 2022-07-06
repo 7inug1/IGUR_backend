0048a827a0b374c46f0d1ab569e3aed8ae7f36f9
jinwookShin <0201jinwook_@gmail.com>
2022-07-06 15:53:38 +0900

    feat: 이미지 cdn url fetching과 base64로 변환 및 리팩토링 추가

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const puppeteer = require("puppeteer");
+const axios = require("axios");
 
 class InstagramUser {
   constructor(profile, contents, locations) {
@@ -20,11 +21,10 @@ router.get("/users/:username", function (req, res, next) {
     (async () => {
       const browser = await puppeteer.launch({ headless: false });
       const page = await browser.newPage();
+
       await page.setViewport({ width: 1920, height: 1080 });
       await page.goto(`https://www.picuki.com/profile/${req.params.username}`, { waitUntil: "networkidle2" });
 
-      // <profile>
-      const content = await page.$("div.wrapper div.content");
       const profileHeader = await page.$(".wrapper .profile-header");
       const username = await profileHeader.$eval(".profile-info .profile-name-top", (element) => {
         return element.textContent;
@@ -32,9 +32,11 @@ router.get("/users/:username", function (req, res, next) {
       const name = await profileHeader.$eval(".profile-info .profile-name-bottom", (element) => {
         return element.textContent;
       });
-      const profileImgSrc = await profileHeader.$eval(".profile-avatar .profile-avatar-image", (element) => {
-        return element.src;
-      });
+      const profileImgSrc = await convertToBase64Img(
+        await profileHeader.$eval(".profile-avatar .profile-avatar-image", (element) => {
+          return element.src;
+        })
+      );
       const profileImgAlt = await profileHeader.$eval(".profile-avatar .profile-avatar-image", (element) => {
         return element.alt;
       });
@@ -51,8 +53,8 @@ router.get("/users/:username", function (req, res, next) {
         return Number(`${element.textContent}`.replaceAll(",", ""));
       });
 
-      // <post>
-      const NUMBER_OF_POSTS_TO_RETRIEVE = 10; // numberOfPosts
+      // const NUMBER_OF_POSTS_TO_RETRIEVE = numberOfPosts; => To fetch all posts
+      const NUMBER_OF_POSTS_TO_RETRIEVE = 10;
       const loadMoreButton = await page.$("button.pagination-failed-retry");
       const locations = {};
       const posts = [];
@@ -65,19 +67,21 @@ router.get("/users/:username", function (req, res, next) {
         await loadMoreButton.evaluate((b) => b.click());
         await page.waitForTimeout(1000);
         retrievedPostsHandles = await page.$$("ul.box-photos li");
-        console.log("retrievedPostsHandles.length", retrievedPostsHandles.length);
       }
+
       let count = 0;
+
       for (let i = NUMBER_OF_POSTS_TO_RETRIEVE; i > 0; i--) {
         const id = numberOfPosts - 1 - count;
         const postElementHandle = retrievedPostsHandles[count];
-        count++;
         const description = await postElementHandle.$eval(".photo-info .photo-description", (element) => {
           return element.textContent.trim();
         });
-        const imgSrc = await postElementHandle.$eval(".photo .post-image", (element) => {
-          return element.src;
-        });
+        const imgSrc = await convertToBase64Img(
+          await postElementHandle.$eval(".photo .post-image", (element) => {
+            return element.src;
+          })
+        );
         const numberOfLikes = await postElementHandle.$eval(".post-footer .likes_photo", (element) => {
           return element.innerText;
         });
@@ -97,6 +101,7 @@ router.get("/users/:username", function (req, res, next) {
         } catch (err) {
           location = null;
         }
+
         const post = {
           id,
           location,
@@ -106,8 +111,11 @@ router.get("/users/:username", function (req, res, next) {
           numberOfReplies,
           datePosted,
         };
+
         contents.posts.push(post);
+        count++;
       }
+
       const profile = {
         username,
         name,
@@ -123,6 +131,57 @@ router.get("/users/:username", function (req, res, next) {
 
       processNaturalLanguage();
     })();
+  }
+
+  // source: https://gist.github.com/jonleighton/958841
+  async function convertToBase64Img(imgSrc) {
+    let base64 = "";
+    const base64encodings = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    try {
+      const res = await axios({ method: "get", url: imgSrc, responseType: "arraybuffer" });
+
+      const bytes = new Uint8Array(res.data);
+      const byteLength = bytes.byteLength;
+      const byteRemainder = byteLength % 3;
+      const mainLength = byteLength - byteRemainder;
+
+      let a, b, c, d;
+      let chunk;
+
+      for (let i = 0; i < mainLength; i = i + 3) {
+        chunk = (bytes[i] << 16) | (bytes[i + 1] << 8) | bytes[i + 2];
+
+        a = (chunk & 16515072) >> 18;
+        b = (chunk & 258048) >> 12;
+        c = (chunk & 4032) >> 6;
+        d = chunk & 63;
+
+        base64 += base64encodings[a] + base64encodings[b] + base64encodings[c] + base64encodings[d];
+      }
+
+      if (byteRemainder === 1) {
+        chunk = bytes[mainLength];
+        a = (chunk & 252) >> 2;
+        b = (chunk & 3) << 4;
+        base64 += base64encodings[a] + base64encodings[b] + "==";
+      } else if (byteRemainder === 2) {
+        chunk = (bytes[mainLength] << 8) | bytes[mainLength + 1];
+
+        a = (chunk & 64512) >> 10;
+        b = (chunk & 1008) >> 4;
+
+        c = (chunk & 15) << 2;
+
+        base64 += base64encodings[a] + base64encodings[b] + base64encodings[c] + "=";
+      }
+    } catch (err) {
+      console.log("err: ", err);
+    }
+
+    base64 = `data:image/jpeg;base64,${base64}`;
+
+    return base64;
   }
 
   async function processNaturalLanguage() {
@@ -148,14 +207,7 @@ router.get("/users/:username", function (req, res, next) {
       post["sentimentsResult"] = sentimentsResult;
       post["entitiesResult"] = entitiesResult;
       post["categoriesResult"] = categoriesResult;
-
-      console.log("post", post);
-      console.log("post.sentimentsResult", post.sentimentsResult);
     }
-
-    // app.get("/", (req, res) => {
-    //   res.send(instagramUser);
-    // });
 
     async function analyzeSentiment(document) {
       let sentimentsResult = null;
