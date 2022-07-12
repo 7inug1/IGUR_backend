6d1afae9e57c42229a6aef032ac36a61a195b7e6
jinwookShin <0201jinwook_@gmail.com>
2022-07-12 11:03:09 +0900

    feat: mongodb  설정 추가

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-unused-vars */
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const index = require("./routes/index");
 const cors = require("cors");
 const PORT = 8000;
+const db = require("./db");
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
