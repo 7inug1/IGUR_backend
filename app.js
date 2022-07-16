0341eaf66dda91bddb9450acbdad5ce036379b1f
jinwookShin <0201jinwook_@gmail.com>
2022-07-16 22:15:25 +0900

    refactor: dotenv에 path 추가

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + '/.env' });
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
