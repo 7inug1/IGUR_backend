a9efbaf3790713023344357faa5fcc5ad3aa8d54
jinwookShin <0201jinwook_@gmail.com>
2022-07-16 22:33:13 +0900

    refactor: db connect 코드 수정

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 require("dotenv").config({ path: __dirname + '/.env' });
+const connectDB = require("./db");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
@@ -11,6 +12,8 @@ const db = require("./db");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
--- db.js
+++ db.js
@@ -1,12 +1,31 @@
+// const mongoose = require("mongoose");
+
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+// });
+
+// const db = mongoose.connection;
+// console.log("db", db);
+
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", console.log.bind(console, "Connected to database.."));
+
+// module.exports = db;
+
 const mongoose = require("mongoose");
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      //must add in order to not get any error masseges:
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true
+    })
+    console.log(`mongo database is connected!!! ${conn.connection.host} `)
+  } catch (error) {
+    console.error(`Error: ${error} `)
+    process.exit(1) //passing 1 - will exit the proccess with error
+  }
+};
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-});
-
-const db = mongoose.connection;
-
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", console.log.bind(console, "Connected to database.."));
-
-module.exports = db;
+module.exports = connectDB;
