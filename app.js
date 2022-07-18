2abead5abc637cdd0604cc1321952e359901bbf3
jinwookShin <0201jinwook_@gmail.com>
2022-07-18 17:38:35 +0900

    refactor: cors header middleware 변경

--- app.js
+++ app.js
@@ -19,9 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
-  const allowedOrigins = "https://igur.vercel.app";
-  const origin = req.headers.origin;
-  console.log("origin", origin);
+  // const allowedOrigins = "https://igur.vercel.app";
+  // const origin = req.headers.origin;
+  // console.log("origin", origin);
   // Website you wish to allow to connect
   // res.setHeader('Access-Control-Allow-Origin', 'https://igur-frontend-7inug1.vercel.app/');
   // if (allowedOrigins.includes(origin)) {
@@ -32,9 +32,6 @@ app.use(function (req, res, next) {
 
   // Request headers you wish to allow
   res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
 
   // Pass to next layer of middleware
   next();
