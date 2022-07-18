244006dec1aaa512a5f80c447b77118b199631fd
jinwookShin <0201jinwook_@gmail.com>
2022-07-18 16:23:16 +0900

    refactor: cors origin 도메인 추가

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 const PORT = 8080;
 
 const corsOptions = {
-  origin: 'https://igur-frontend-7inug1.vercel.app/',
+  origin: ['https://igur.vercel.app/', 'https://igur-frontend-7inug1.vercel.app/', 'https://igur-frontend-git-master-7inug1.vercel.app/'],
   credentials: true,
   optionsSuccessStatus: 200
 };
@@ -18,22 +18,25 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Add headers before the routes are defined
 app.use(function (req, res, next) {
+  const allowedOrigins = ['https://igur.vercel.app/', 'https://igur-frontend-7inug1.vercel.app/', 'https://igur-frontend-git-master-7inug1.vercel.app/'];
+  const origin = req.headers.origin;
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'https://igur-frontend-7inug1.vercel.app/');
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://igur-frontend-7inug1.vercel.app/');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
 
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
+  // Pass to next layer of middleware
+  next();
 });
 connectDB();
 
