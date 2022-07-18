4a8d868bd40dec311216aa24de1a1c0b92f7aba0
jinwookShin <0201jinwook_@gmail.com>
2022-07-18 16:30:04 +0900

    refactor: cors origin wildcard로 변경

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ const index = require("./routes/index");
 const cors = require("cors");
 const PORT = 8080;
 
-const corsOptions = {
-  origin: ['https://igur.vercel.app/', 'https://igur-frontend-7inug1.vercel.app/', 'https://igur-frontend-git-master-7inug1.vercel.app/'],
-  credentials: true,
-  optionsSuccessStatus: 200
-};
-app.use(cors(corsOptions));
+// const corsOptions = {
+//   origin: ['https://igur.vercel.app/', 'https://igur-frontend-7inug1.vercel.app/', 'https://igur-frontend-git-master-7inug1.vercel.app/'],
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// };
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Add headers before the routes are defined
