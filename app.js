ad3335f5524292c4da5eca3c582d3fa2098e57a3
jinwookShin <0201jinwook_@gmail.com>
2022-07-18 16:34:36 +0900

    refactor: cors middleware 주석처리

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 const index = require("./routes/index");
-const cors = require("cors");
+// const cors = require("cors");
 const PORT = 8080;
 
 // const corsOptions = {
@@ -13,32 +13,33 @@ const PORT = 8080;
 //   credentials: true,
 //   optionsSuccessStatus: 200
 // };
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Add headers before the routes are defined
-// app.use(function (req, res, next) {
-//   const allowedOrigins = ['https://igur.vercel.app/', 'https://igur-frontend-7inug1.vercel.app/', 'https://igur-frontend-git-master-7inug1.vercel.app/'];
-//   const origin = req.headers.origin;
-//   // Website you wish to allow to connect
-//   // res.setHeader('Access-Control-Allow-Origin', 'https://igur-frontend-7inug1.vercel.app/');
-//   if (allowedOrigins.includes(origin)) {
-//     res.setHeader('Access-Control-Allow-Origin', origin);
-//   }
-//   // Request methods you wish to allow
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-//   // Request headers you wish to allow
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-//   // Set to true if you need the website to include cookies in the requests sent
-//   // to the API (e.g. in case you use sessions)
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-
-//   // Pass to next layer of middleware
-//   next();
-// });
 connectDB();
+app.use(function (req, res, next) {
+  const allowedOrigins = ['https://igur.vercel.app/', 'https://igur-frontend-7inug1.vercel.app/', 'https://igur-frontend-git-master-7inug1.vercel.app/'];
+  const origin = req.headers.origin;
+  console.log("origin", origin);
+  // Website you wish to allow to connect
+  // res.setHeader('Access-Control-Allow-Origin', 'https://igur-frontend-7inug1.vercel.app/');
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
