28cae69e325bfd82202e6ad4b522781a9cbb565c
jinwookShin <0201jinwook_@gmail.com>
2022-07-18 18:03:09 +0900

    refactor: cors 코드 변경

--- app.js
+++ app.js
@@ -18,27 +18,35 @@ app.use(cors({
   "origin": "*",
   "credentials": true,
 }));
-app.use(function (req, res, next) {
-  // const allowedOrigins = "https://igur.vercel.app";
-  // const origin = req.headers.origin;
-  // console.log("origin", origin);
-  // Website you wish to allow to connect
-  // res.setHeader('Access-Control-Allow-Origin', 'https://igur-frontend-7inug1.vercel.app/');
-  // if (allowedOrigins.includes(origin)) {
-  res.setHeader('Access-Control-Allow-Origin', "https://igur.vercel.app");
-
-  // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
-
-  // }
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader("Access-Control-Allow-Headers", "*");
-
-  // Pass to next layer of middleware
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://igur.vercel.app");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
+// app.use(function (req, res, next) {
+//   // const allowedOrigins = "https://igur.vercel.app";
+//   // const origin = req.headers.origin;
+//   // console.log("origin", origin);
+//   // Website you wish to allow to connect
+//   // res.setHeader('Access-Control-Allow-Origin', 'https://igur-frontend-7inug1.vercel.app/');
+//   // if (allowedOrigins.includes(origin)) {
+//   res.setHeader('Access-Control-Allow-Origin', "https://igur.vercel.app");
+
+//   // res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+
+//   // }
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+//   // Request headers you wish to allow
+//   res.setHeader("Access-Control-Allow-Headers", "*");
+
+//   // Pass to next layer of middleware
+//   next();
+// });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // Add headers before the routes are defined
