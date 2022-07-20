e955d41600423e5ee1a5c30c132eb3c677545fc7
jinwookShin <0201jinwook_@gmail.com>
2022-07-20 16:25:11 +0900

    refactor: cors 수정

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 require("dotenv").config({ path: __dirname + '/.env' });
 const connectDB = require("./db");
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const index = require("./routes/index");
@@ -15,28 +15,28 @@ connectDB();
 //   origin: "https://igur.vercel.app",
 //   methods: ['GET', 'POST', 'PATCH'],
 // }));
-// app.use(cors());
+app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(function (req, res, next) {
+// app.use(function (req, res, next) {
 
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://igur.vercel.app');
+//   // Website you wish to allow to connect
+//   res.setHeader('Access-Control-Allow-Origin', 'https://igur.vercel.app');
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//   // Request methods you wish to allow
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  // res.setHeader('Content-Type', 'application/json');
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+//   // Request headers you wish to allow
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+//   // res.setHeader('Content-Type', 'application/json');
+//   // Set to true if you need the website to include cookies in the requests sent
+//   // to the API (e.g. in case you use sessions)
+//   res.setHeader('Access-Control-Allow-Credentials', true);
 
-  // Pass to next layer of middleware
-  next();
-});
+//   // Pass to next layer of middleware
+//   next();
+// });
 
 app.listen(PORT, () => {
   console.log("listening on port " + PORT);
